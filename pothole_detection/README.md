9889830558b87c45c9de3a8fc7afba25f9d44aad
Satyajeet Narayan <56536469+Satyajeet-code@users.noreply.github.com>
2024-04-24 14:49:39 +0530

    Add files via upload

---
+++ pothole_detection/README.md
@@ -0,0 +1,21 @@
+# Pothole Detection
+
+🤖 **Welcome to the pothole detection project!** This project takes in an image as an input and predicts if it is a normal road image or an image of road containing potholes.
+
+
+## Usage
+1. Run `requirements.txt` to install the dependencies.
+
+```bash
+pip install -r requirements.txt
+```
+2. Execute `pothole_detection_app.py`  to open up the UI using the following command:
+
+```bash
+streamlit run pothole_detection_app.py
+```
+Replace cnn.h5 in the code with the path to cnn.h5 in your computer.
+
+### Screenshot
+
+![App Screenshot](https://github.com/Satyajeet-code/Generative-AI/blob/main/Translator_and_summarizer/Screenshot%202024-04-24%20123935.png)
---
+++ pothole_detection/pothole_detection_app.py
@@ -0,0 +1,36 @@
+# from tensorflow.keras.models import Model
+import numpy as np 
+import tensorflow as tf
+from keras.preprocessing import image
+import streamlit as st
+
+st.set_page_config(page_title="Pothole detection")
+st.header("Pothole detection")
+
+
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+# image=""  
+submit=st.button("Predict")
+def image_prediction(uploaded_file):
+    model= tf.keras.models.load_model("cnn.h5") 
+    test_image = image.load_img(uploaded_file, target_size = (64, 64))
+    # plt.imshow(cv2.imread("D:/Python/Pothole detection/potholes/123.jpg"))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    result = model.predict(test_image)
+
+    if result[0][0] == 1:
+        prediction = 'pothole'
+    else:
+        prediction = 'normal'
+
+
+    print(prediction)
+    return prediction
+
+if submit:
+    pred=image_prediction(uploaded_file)
+    st.image(uploaded_file)
+    st.subheader("Prediction:")
+    st.write(pred)
