bfeab70ba12179d7acbf4bafcfc8bda3e0364151
Satyajeet Narayan <56536469+Satyajeet-code@users.noreply.github.com>
2024-04-24 15:02:31 +0530

    Update pothole_detection_app.py

--- pothole_detection/pothole_detection_app.py
+++ pothole_detection/pothole_detection_app.py
@@ -13,7 +13,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 # image=""  
 submit=st.button("Predict")
 def image_prediction(uploaded_file):
-    model= tf.keras.models.load_model("cnn.h5") 
+    model= tf.keras.models.load_model("pothole_detection/cnn.h5") 
     test_image = image.load_img(uploaded_file, target_size = (64, 64))
     # plt.imshow(cv2.imread("D:/Python/Pothole detection/potholes/123.jpg"))
     test_image = image.img_to_array(test_image)
